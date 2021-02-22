bd52750cfffd12e9f0b3848fe8e1a500d7d3bf77
Estiven Padilla <estivenps26@gmail.com>
2021-02-21 23:18:16 -0400

    Subiendo repositorio

--- Simulador_Compuertas_Logicas/MainForm.Designer.cs
+++ Simulador_Compuertas_Logicas/MainForm.Designer.cs
@@ -165,7 +165,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(531, 280);
+            this.label2.Location = new System.Drawing.Point(530, 280);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 24);
             this.label2.TabIndex = 18;
